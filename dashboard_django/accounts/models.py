d09d5be3dc66169f495b724033230f24cdaade25
TwikeySen <tomas.gonzalez.vega@gmail.com>
2020-08-18 19:32:35 +0200

    Adding relations to models manytoone and one to many

--- dashboard_django/accounts/models.py
+++ dashboard_django/accounts/models.py
@@ -11,7 +11,15 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-class Products(models.Model):
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
         ('Out door', 'Out door'),
@@ -19,8 +27,12 @@ class Products(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS =(
@@ -28,7 +40,7 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null= True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
