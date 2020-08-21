27578c0b10be7e238326b4292e499046846335f1
TwikeySen <dontgonzalezvega@gmail.com>
2020-08-21 22:27:33 +0200

    Adding documentation for urls file

--- dashboard_django/accounts/urls.py
+++ dashboard_django/accounts/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from . import views 
 
+"""
+    In this file we add all url that link into views.
+    urlpatterns is a list.
+    path is a method from django.urls
+    We specify the path then the view (that is a method) then the name of the path.
+"""
+
 urlpatterns = [
     path('', views.home, name="home"),
+    
     path('products/', views.products, name="products"),
+    
     path('customer/<str:pk>/', views.customer, name="customer"),
-    path('create_order/', views.createOrder, name="create_order"),
+    
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 ]
--- dashboard_django/accounts/views.py
+++ dashboard_django/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import *
 
@@ -38,16 +39,20 @@ def customer(request, pk):
 
     return render(request, 'accounts/customer.html', data)
 
-
-def createOrder(request):
-    form = OrderForm()
+# TODO I NEED TO CHECK OUT THIS AGAIN .. 
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status')) # Need to gives the parent and child model
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(instance=customer)
+    #form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
         if form.is_valid:
-            form.save() # Model form will handle the saving inside the database
+            formset.save() # Model form will handle the saving inside the database
             return redirect('/')
 
-    data = {'form' : form}
+    data = {'formset' : formset}
 
     return render(request, 'accounts/order_form.html', data)
 
