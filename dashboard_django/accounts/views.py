de71609b318728545ba9b3bb075326bd7cc1b82f
TwikeySen <dontgonzalezvega@gmail.com>
2020-08-24 14:37:15 +0200

    Handling login page with logic

--- dashboard_django/accounts/views.py
+++ dashboard_django/accounts/views.py
@@ -35,9 +35,18 @@ def registerPage(request):
 def loginPage(request):
     
     if request.method == 'POST':
-        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
-    return render(request, 'accounts/login.html')
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
