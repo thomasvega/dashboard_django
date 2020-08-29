183927b360d820f218b13285aabb68f834b1b191
TwikeySen <dontgonzalezvega@gmail.com>
2020-08-29 15:44:09 +0200

    Single user profile page

--- dashboard_django/accounts/views.py
+++ dashboard_django/accounts/views.py
@@ -87,9 +87,16 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', datas)
 
-
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all() 
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders,'total_orders': total_orders, 'delivered': delivered, 'pending': pending,}
     return render(request, 'accounts/user.html', context)
 
 
